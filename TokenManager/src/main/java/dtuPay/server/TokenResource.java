facf9aa00af499c053b37921d1168c39af291547
s202772 <s202772@student.dtu.dk>
2022-01-11 17:03:14 +0100

    Tokens dummies

---
+++ TokenManager/src/main/java/dtuPay/server/TokenResource.java
@@ -0,0 +1,26 @@
+package dtuPay.server;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+@Path("/Tokens")
+public class TokenResource {
+
+
+    @POST
+    @Path("/Tokens")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response generateTokens(DtuPayUser user) {
+        /** Filled with w/e things that should be done different
+         * */
+        String newMid = null;
+        try {
+            return Response.ok().entity(newMid).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+        }
+    }
+
+
+}
