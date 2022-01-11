396914faba7af05daafb05fe5a3a26fbfaa256b3
Kasper <kasper.b.joergensen@gmail.com>
2022-01-11 16:21:53 +0100

    initial commit

---
+++ PaymentManager/src/test/java/dtuPay/server/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package dtuPay.server;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
