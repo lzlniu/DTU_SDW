cad6755cd4b1113b722d83171a06fa384cd7ab72
lzlniu <lzlniu@qq.com>
2022-01-13 15:16:59 +0100

    fix order of build and run script

--- build_and_run_all.sh
+++ build_and_run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 #docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management
-for i in $(ls ./*/build_*.sh); do
+for i in $(ls ./*/build_*.sh | sort -r); do
 	bash ${i}
 done
