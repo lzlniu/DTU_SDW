99e791d9c7305e388b68828023cfb5761861ed74
lzlniu <lzlniu@qq.com>
2022-01-17 12:15:11 +0100

    change depends' name back in docker-compose.yml

--- build_and_run_all.sh
+++ build_and_run_all.sh
@@ -8,9 +8,8 @@ for i in $(ls -d *Manager); do
   bash build_*.sh
   cd ..
 done #build all the stuffs
-docker-compose up -d rabbitMq
-#sleep 5
-docker-compose up -d accountmanager_service \
+docker-compose up -d rabbitMq \
+accountmanager_service \
 paymentmanager_service \
 reportmanager_service \
 tokenmanager_service
