797ef14900adca8f70374b4ce2526849e876ad37
lzlniu <lzlniu@qq.com>
2022-01-17 11:53:05 +0100

    update build and run scripts

--- PaymentManager/build_pay.sh
+++ PaymentManager/build_pay.sh
@@ -2,4 +2,4 @@
 set -e
 mvn clean package
 mv ./target/quarkus-app/quarkus-run.jar ./target/quarkus-app/payment.jar
-docker-compose --build paymentmanager_service
+docker-compose build paymentmanager_service
--- ReportManager/build_rep.sh
+++ ReportManager/build_rep.sh
@@ -2,4 +2,4 @@
 set -e
 mvn clean package
 mv ./target/quarkus-app/quarkus-run.jar ./target/quarkus-app/report.jar
-docker-compose --build reportmanager_service
+docker-compose build reportmanager_service
