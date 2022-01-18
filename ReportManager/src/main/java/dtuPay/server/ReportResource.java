7940cd8d13a5236714fe1e4a20e2d17d38927ca5
Kasper <kasper.b.joergensen@gmail.com>
2022-01-18 16:22:54 +0100

    Re-added accidentally deleted }

--- ReportManager/src/main/java/dtuPay/server/ReportResource.java
+++ ReportManager/src/main/java/dtuPay/server/ReportResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.core.Response;
 @Path("/reports")
 public class ReportResource {
     ReportManager report = new ReportManagerFactory().getManager();
+
     //@author s212643 - Xingguang Geng
     @GET
     @Path("/customers/{id}")
@@ -17,6 +18,7 @@ public class ReportResource {
     public Set<Payment> getCustomerReport(@PathParam("id") String customer) throws Exception {
         return report.getCustomerPayments(customer);
     }
+
     //@author s164422 - Thomas Bergen
     @GET
     @Path("/merchants/{id}")
@@ -24,9 +26,11 @@ public class ReportResource {
     public Set<Payment> getMerchantReport(@PathParam("id") String merchant) throws Exception {
         return report.getMerchantPayments(merchant);
     }
+
     //@author s174293 - Kasper Jørgensen
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Set<Payment> getSuperReport() throws Exception {
         return report.getPayments();
     }
+}
